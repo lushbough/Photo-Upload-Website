a3805a07a2665c86b5e38154edd46e7b521972a4
William <lushbough@gmail.com>
2020-07-24 13:40:25 -0700

    changed home and indexjs

--- application/routes/index.js
+++ application/routes/index.js
@@ -3,18 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+  res.render('index', {title:"Image App"});
 });
 router.get('/login.hbs',(req, res, next)=> {
-  res.render("login");
+  res.render("login", {title:"Login"});
 });
 router.get('/registration.hbs',(req, res, next) => {
-  res.render("registration");
+  res.render("registration", {title:"Registration"});
 });
 router.get('/postimage.hbs',(req, res, next) => {
-  res.render("postimage");
+  res.render("postimage", {title:"Post Image"});
 });
 router.get('/imagepost.hbs',(req, res, next) => {
-  res.render("imagepost");
+  res.render("imagepost", {title:"Image Post"});
 });
 module.exports = router;
